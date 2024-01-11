a4dfd128ef4b96990f4d1bba24ff5eda45c1e34b
Michal Rzepa <michalrzepa@icloud.com>
2024-01-11 15:53:10 +0100

    Add itemModel

---
+++ lib/models/item_model.dart
@@ -0,0 +1,13 @@
+class ItemModel {
+  ItemModel({
+    required this.tittle,
+    required this.imageURL,
+    required this.relaseDate,
+    required this.id,
+  });
+
+  final String tittle;
+  final String imageURL;
+  final DateTime relaseDate;
+  final String id;
+}
